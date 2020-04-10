b92709ccd691d3a8127c92b5dce892467aa4a272
qhwa <qhwa@163.com>
2020-04-10 13:44:55 +0800

    init commit

---
+++ lib/ucwidth.ex
@@ -0,0 +1,18 @@
+defmodule Ucwidth do
+  @moduledoc """
+  Documentation for `Ucwidth`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ucwidth.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ucwidth_test.exs
@@ -0,0 +1,8 @@
+defmodule UcwidthTest do
+  use ExUnit.Case
+  doctest Ucwidth
+
+  test "greets the world" do
+    assert Ucwidth.hello() == :world
+  end
+end
